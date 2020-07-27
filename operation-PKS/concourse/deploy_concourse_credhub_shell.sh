ff3192579ddecae0ca6c0e8250a6d86baeb08add
DaeBin HA <dbha@sk.com>
2020-07-27 09:40:40 +0900

    update

---
+++ operation-PKS/concourse/deploy_concourse_credhub_shell.sh
@@ -0,0 +1,15 @@
+bosh deploy \
+-d concourse ../concourse-bosh-deployment/cluster/concourse.yml \
+-l ../concourse-bosh-deployment/versions.yml \
+-l variables.yml \
+-l concourse-credhub-vars.yml \
+--vars-store cluster-creds.yml \
+-o ../concourse-bosh-deployment/cluster/operations/backup-atc.yml \
+-o ../concourse-bosh-deployment/cluster/operations/basic-auth.yml \
+-o ../concourse-bosh-deployment/cluster/operations/privileged-http.yml \
+-o ../concourse-bosh-deployment/cluster/operations/static-web.yml \
+-o ../concourse-bosh-deployment/cluster/operations/privileged-https.yml \
+-o ../concourse-bosh-deployment/cluster/operations/tls.yml \
+-o ../concourse-bosh-deployment/cluster/operations/tls-vars.yml  \
+-o ../concourse-bosh-deployment/cluster/operations/worker-ephemeral-disk.yml \
+-o ../concourse-bosh-deployment/cluster/operations/credhub.yml
